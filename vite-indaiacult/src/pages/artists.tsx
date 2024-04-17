40092993daa598c2fd62b60fa723b8794b35d91b
joão siqueira <joaosiqueirapy@gmail.com>
2024-04-16 21:21:46 -0300

    small changes on artist's page

--- vite-indaiacult/src/pages/artists.tsx
+++ vite-indaiacult/src/pages/artists.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const ArtistsPage = () => {
-  return <div>ArtistsPage</div>;
+  return (
+    <div className="flex justify-center items-center">
+      <div className="flex text-2xl font-semibold font-montserrat dark:text-white">
+        Artistas
+      </div>
+    </div>
+  );
 };
 
 export default ArtistsPage;
