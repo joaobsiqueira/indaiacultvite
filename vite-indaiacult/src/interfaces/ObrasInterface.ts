219b0ccee9a6f2369186af5ab6ed4fed93df8031
João Siqueira <joaosiqueirapy@gmail.com>
2024-04-30 20:54:56 -0300

    feat: obra mockup & interface

---
+++ vite-indaiacult/src/interfaces/ObrasInterface.ts
@@ -0,0 +1,8 @@
+export interface Obras {
+  id: string;
+  name: string;
+  category: string;
+  description: string;
+  image: string;
+  artista: string;
+}
---
+++ vite-indaiacult/src/obrasMock.ts
@@ -0,0 +1,16 @@
+import { Obras } from "./interfaces/ObrasInterface";
+import { Artist } from "./interfaces/ArtistInterface";
+import { artists } from "./mock";
+
+const Obra: Obras[] = [
+  {
+    id: "1",
+    name: "caminho das rosas",
+    category: "pintura",
+    description:
+      "Uma obra feita para tentar retratar meu sentimento ao passar pelo campo de rosas perto da minha casa, realmente é muito legal e emocionante. Amo rosas e amo a vida",
+    image:
+      "https://i.pinimg.com/originals/dc/ed/d1/dcedd10081861c1c60e0869da0db5eb5.jpg",
+    artista: "José Paulo",
+  },
+];
