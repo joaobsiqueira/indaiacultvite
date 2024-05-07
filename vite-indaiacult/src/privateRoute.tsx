a5e2d23b11f45726082f56afe58387daea535983
João Siqueira <joaosiqueirapy@gmail.com>
2024-05-06 21:20:05 -0300

    user context, and private routes for the ones who are not logged in

---
+++ vite-indaiacult/src/privateRoute.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import { useUser } from "./userContext";
+
+const PrivateRoute: React.FC<{ element: React.ReactNode; path: string }> = ({
+  element,
+  path,
+}) => {
+  const { user, artist } = useUser();
+
+  const isAuthenticated = user !== null || artist !== null;
+
+  return (
+    <Routes>
+      <Route
+        path={path}
+        element={isAuthenticated ? element : <Navigate to="/login" replace />}
+      />
+    </Routes>
+  );
+};
+
+export default PrivateRoute;
---
+++ vite-indaiacult/src/userContext.tsx
@@ -0,0 +1,47 @@
+import React, { createContext, ReactNode, useContext, useState } from "react";
+
+interface User {
+  id: string;
+  name: string;
+  img: string;
+  email: string;
+}
+
+interface Artist extends User {
+  genre: string;
+  description: string;
+  image: string;
+  banner: string;
+}
+
+interface UserContextType {
+  user: User | null;
+  artist: Artist | null;
+  setUser: (user: User | null) => void;
+  setArtist: (artist: Artist | null) => void;
+}
+
+const UserContext = createContext<UserContextType | undefined>(undefined);
+
+export const UserProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<User | null>({
+    email: "",
+    id: "",
+    img: "",
+    name: "aaaa",
+  });
+  const [artist, setArtist] = useState<Artist | null>(null);
+  return (
+    <UserContext.Provider value={{ user, setUser, artist, setArtist }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUser = () => {
+  const context = useContext(UserContext);
+  if (!context) {
+    throw new Error("useUser must be used within a UserProvider");
+  }
+  return context;
+};
