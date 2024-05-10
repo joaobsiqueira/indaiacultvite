535e9948a3d33c5949dc0b6adc4b2bd1a2f4969c
joao siqueira <joaosiqueirapy@gmail.com>
2024-05-09 22:52:43 -0300

    fix: darkmode and small style changes

--- vite-indaiacult/src/components/LandingNav.tsx
+++ vite-indaiacult/src/components/LandingNav.tsx
@@ -1,19 +1,27 @@
 import { Link } from "react-router-dom";
+import ToggleThemeButton from "./ToggleThemeButton";
 
 const LandingNav = () => {
   return (
     <section>
       <div className="flex items-center justify-between p-5">
         <div>
-          <span className="text-main text-3xl font-bold">Indaia</span>
-          <span className="text-lightblue text-3xl font-bold">Cult.</span>
+          <Link to="/">
+            <span className="text-main text-3xl font-bold">Indaia</span>
+            <span className="dark:text-lightblue text-darkblue text-3xl font-bold">
+              Cult.
+            </span>
+          </Link>
         </div>
-        <Link
-          to="/sobre"
-          className="font-montserrat text-lg md:text-2xl font-bold text-darkblue hover:underline"
-        >
-          Sobre
-        </Link>
+        <div className="flex justify-end items-center space-x-6">
+          <Link
+            to="/sobre"
+            className="font-montserrat text-lg md:text-2xl font-bold text-darkblue dark:text-lightblue hover:underline"
+          >
+            Sobre
+          </Link>
+          <ToggleThemeButton />
+        </div>
       </div>
     </section>
   );
